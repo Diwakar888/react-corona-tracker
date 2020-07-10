ea9198d5346ea0e1b612c2b14e653baa9b2495ce
Diwakar888 <diwakar134@gmail.com>
2020-07-10 17:46:31 +0530

    things related to chart component removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // import Cards from './components/Cards/Cards'
-import {Cards,Chart,CountryPicker} from './components';
+import {Cards,CountryPicker} from './components';
 import styles from './App.module.css'
 import { fetchData } from './api';
 
